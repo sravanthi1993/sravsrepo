201c2d21dd3848230eb640a3036e917372a08da9
Sai Guru Datt Manchikanti <samanchi@microsoft.com>
2021-03-08 17:33:29 -0800

    aad

--- projections/src/App.js
+++ projections/src/App.js
@@ -7,6 +7,8 @@ import { BrowserRouter } from 'react-router-dom'
 import { Link, Label } from 'office-ui-fabric-react';
 import { BrowserRouter as Switch, Route} from 'react-router-dom'
 import './App.css';
+import {AzureAD, AuthenticationState} from 'react-aad-msal'
+import {authProvider} from './AuthProvider'
 
 function App() {
 
@@ -21,6 +23,14 @@ function App() {
      <Link href="/addresource"  className="Content"> AddResource</Link> |
      <Link href="/search"  className="Content"> Search</Link> |
      <Link href="/holidays" className ="Content">Holidays</Link>
+     <AzureAD
+      provider={authProvider}>
+      {({ login, logout, authenticationState }) => {
+        if (authenticationState === AuthenticationState.Authenticated) {
+          return (<button className = "logout" onClick={logout} color="inherit" variant="outlined">Log Out</button>);
+        }
+      }}
+    </AzureAD>
      </div>
      
          <Switch>
